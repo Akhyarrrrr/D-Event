c22fd48d742869132ea73ecf5466c22dd6bde5cf
Akhyar <akhyar1@mhs.unsyiah.ac.id>
2024-05-07 13:46:30 +0700

    first commit

---
+++ D'Event/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./resources/**/*.edge', './resources/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
