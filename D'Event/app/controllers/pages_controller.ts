6c40ac9205a5e4ceef77fe2caea6bfc6bbc298d5
Akhyar <akhyar1@mhs.unsyiah.ac.id>
2024-05-12 12:38:05 +0700

    add login, sign up and pagescontroller

---
+++ D'Event/app/controllers/pages_controller.ts
@@ -0,0 +1,31 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class PagesController {
+  async beranda({ view }: HttpContext) {
+    return view.render('pages/beranda')
+  }
+
+  async acara({ view }: HttpContext) {
+    return view.render('pages/acara')
+  }
+
+  async bantuan({ view }: HttpContext) {
+    return view.render('pages/bantuan')
+  }
+
+  async blog({ view }: HttpContext) {
+    return view.render('pages/blog')
+  }
+
+  async login({ view }: HttpContext) {
+    return view.render('pages/login')
+  }
+
+  async register({ view }: HttpContext) {
+    return view.render('pages/register')
+  }
+
+  async selengkapnya({ view }: HttpContext) {
+    return view.render('pages/selengkapnya')
+  }
+}
